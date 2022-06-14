0c2b1939a2864a6ff7c6b6947f597f8ce4eceee8
Alalinga Mubarik <alalinga.mubarik@amalitech.com>
2022-06-14 12:11:51 +0000

    changed whole code from normal funtions to a class

--- src/utils/user.js
+++ src/utils/user.js
@@ -1,21 +1,44 @@
 //store user temporaly
-const users = []
+// const allUsers = []
+// const users = []
+// const privateChat_Users = []
 
-const messageFormat = (user,message)=>{
-    return{
-        user,
-        message,
+class User {
+    constructor() {
+        this.allUsers = []
+        this.user = []
+        this.privateChat_Users = []
+    }
+
+    privateChatMessageFormat(message,to,from) {
+        return {
+            message,
+            to,
+            from,
+        }
+    }
+    messageFormat = (user, message) => {
+        return {
+            user,
+            message,
+        }
+    }
+
+    addUserToGroup(id, username, group) {
+        if (username, this.user.find(user => user.username === username) === undefined) this.user.push({ id, username, group })
+    }
+
+    getUser(id) {
+        return this.user.find(user => user.id === id)
+    }
+    getGroupUsers(group) {
+        return this.user.filter(user => user.group === group)
+    }
+    addUsers(user) {
+        if (this.allUsers.find(u => u.userName === user.userName) === undefined) this.allUsers.push({ userId: user.userId, userName: user.userName })
+    }
+    getAllUsers() {
+        return this.allUsers
     }
 }
-
-const addUsers = (id,username,group)=>{
-   if(username,users.find(user=>user.username===username)===undefined) users.push({id,username,group})
-}
-
-const getUser = (id)=>{
-    return users.find(user=>user.id===id)
-}
-const getGroupUsers = (group)=>{
-    return users.filter(user=>user.group===group)
-}
-module.exports = {messageFormat,addUsers,getUser,getGroupUsers}
+module.exports = { User }
