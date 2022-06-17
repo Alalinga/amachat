5bf880860f5e9d9df8cca1fbfbc46c69da56f145
Alalinga Mubarik <alalinga.mubarik@amalitech.com>
2022-06-17 09:22:47 +0000

    modified diplay chat functions and added getTime function

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -5,16 +5,15 @@ const form = document.getElementById('form');
 const input = document.getElementById('input');
 let messages = document.getElementById('messages');
 const group_Members = document.getElementById('users');
+const chatList = document.getElementById('chat');
+
 const { username, group } = Qs.parse(location.search, { ignoreQueryPrefix: true })
-let mySelf = null;
 
-
-// const onUserSelectListerner = (username) => {
-//     //isUserSelected = true;
-//     socket.auth = { username };
-//     socket.connect()
-// }
-
+const getTime =()=>{
+    const date = new Date();
+    const minutes = date.getMinutes() <10? '0'+date.getMinutes():date.getMinutes()
+    return date.getHours()+':'+minutes
+}
 const getCurrentUser = (user, id) => {
     return user.find(u => u.userId === id)
 }
@@ -22,25 +21,62 @@ const displayUsers = (users) => {
 
     const userList = `${users.map(user => `<li class='group-list'><a href='private.html?username=${user.userName}&type=private?'>${user.userName}<a> </li>`).join('')}`
 }
+
+//at first entry connect user using username
 if (username && group) {
-    //onUserSelectListerner(username)
-    socket.auth = { username };
+    //const userId = localStorage.getItem('userId')
+    socket.auth = {username};
+    //socket.username = username;
     socket.connect();
-    socket.emit('joinGroup',{username,group});
+    socket.emit('joinGroup', { username, group });
+    
 } else {
     window.location.href = 'index.html';
 }
+
+const displayMyChats = (chats) => {
+
+    const list = document.createElement('li');
+    list.classList.add('self');
+    const chat = ` <div class="avatar"><img src="https://i.imgur.com/DY6gND0.png" draggable="false" /></div><div class="msg"><p style="color:black;font-weight:bold; font-size:12px">${chats}</p><time>${getTime()}</time></div>`
+    list.innerHTML = chat
+    chatList.appendChild(list);
+    window.scrollTo(0, chatList.scrollHeight)
+}
+const displayFriendChats = (chats) => {
+    console.log(chats)
+    const list = document.createElement('li');
+    list.classList.add('other');
+    const chat = ` <div class="avatar"><img src="https://i.imgur.com/DY6gND0.png" draggable="false" /></div><div class="msg"><p style="color:#d9534f">${chats.user}</p><p style="color:black;font-weight:bold; font-size:12px">${chats.message}</p><time>${chats.time}</time></div>`
+    list.innerHTML = chat
+    chatList.appendChild(list);
+    window.scrollTo(0, chatList.scrollHeight)
+
+}
+
+
 form.addEventListener('submit', (e) => {
     e.preventDefault()
     if (input.value) {
         //sedning message to server
-        socket.emit('sendChat', input.value)
+        socket.emit('sendChat', input.value);
+        displayMyChats(input.value)
         input.value = ''
         input.focus()
     }
 })
 
+//After user been connect listen for user data and store it to localStorage and add userId to socket auth
+socket.on('userInfo',(user)=>{
+    const userId = user.userId
+    socket.auth={userId}
+    localStorage.setItem('userId',userId)
+    socket.userId =localStorage.getItem('userId');
+});
 
+socket.on('userDisconnected',(user)=>{
+    console.log(user,'disconnected')
+})
 socket.on('connection_err', (err) => {
     if (err.message.includes('valid username')) {
         // do sothing if there is an error relating to user
@@ -51,10 +87,7 @@ socket.on('connection_err', (err) => {
 //     console.log('We got message',message)
 // })
 socket.on('message', (msg) => {
-    var item = document.createElement('li')
-    item.textContent = msg.user + '=>' + msg.message
-    messages.appendChild(item)
-    window.scrollTo(0, document.body.scrollHeight)
+    displayFriendChats(msg)
 
 });
 
@@ -65,29 +98,21 @@ socket.on('joinAlert', (message) => {
 
 });
 socket.on('joinMembers', (members) => {
-    console.log(members)
     groupMenbers(members.users)
-
-    // alert(message.user+' '+message.message)
-    //groupMenbers(message.user)
 
 });
 
-// socket.on('users', (users) => {
-//     console.log(users)
-//     const cUser = getCurrentUser(users, socket.id)
-//     users = users.splice(users.indexOf(cUser), 1)
-//     displayUsers(users)
-
-// })
-
 
 const groupMenbers = (members) => {
-    // var item = document.createElement('li')
-    // for(let member of members){
-    //     item.textContent =member.username   
+    // const id = localStorage.getItem('userId');
+    // const u = members.find(user=>user.userId===id);
+    // console.log('Bfore',members)
+    // if(u!==-1){
+    // const newmembers= members.splice(members.indexOf(u),1)
+    // console.log('After',newmembers)
     // }
     group_Members.innerHTML = `${members.map(user => `<li class='group-list'><a href='private.html?username=&to=${user.username}&type=private?'>${user.username}<a> </li>`).join('')}`
+    
 }
 
 
