8da935a89d62d03750a15495254d7269d35a6362
Alalinga <alalinga.mubarik@amalitech.com>
2022-06-29 08:34:33 +0000

    build: added cross origin resource sharing (cors),bodyparser and imported api router

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const path = require('path')
 const app = express()
 const server = require('http').createServer(app)
@@ -7,60 +8,53 @@ const bodyParser = require('body-parser')
 const PORT = 5000 || process.env.PORT
 const io = new Server(server)
 //const chatRouter = require('./routes/chatRouters')(io)
-const { ioModule, chatRouter } = require('./routes/chatRouters')
+const { chatRouter } = require('./api/chatRouters')
 const { chat } = require('./chats/chat')
 const { StoreSessions } = require('./utils/sessionstore')
 const { User } = require('./utils/user')
 const sessionStore = new StoreSessions()
 const newUser = new User
+app.use(cors())
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.json())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-// app.use(express.session({
-//     store: sessionstore.createSessionStore()
-// }))
 
-app.use('/api', chatRouter)
+//ioModule(io)
+app.use('', chatRouter)
 io.use((socket, next) => {
-    //check if user has session infor
+
     const userId = socket.handshake.auth.userId;
     const username = socket.handshake.auth.username;
-    // console.log('username',username)
+    // when user refreshes the page, socket need not generate a new id again, we get the old id and fetch it data from the databse aka session
     if (userId) {
         const session = sessionStore.getSession(userId);
 
+        //If the id provided is in the database aka session, then set it to socket for communication
+        // at this point we dont want socket to use it newly generated id for the connection
         if (session) {
-            // console.log('second time with data ', sessionStore.getAllSessions())
             socket.userId = session.userId;
             socket.username = session.username;
             return next()
         }
-        // this code is here because anytime the server get refreshed the session get cleared meanwhile we still have user id and name so we store those in the session again and proceed
-        // NOTE: this code can be removed when using real database, 
-        // socket.userId = userId;
-        // //socket.username = username;
-        // sessionStore.saveSession(socket.userId, { userId: socket.userId, username: socket.username })
-        // console.log('second time but no session',socket.handshake.auth.username, sessionStore.getAllSessions())
-        // return next()
+
     }
 
-    // if first connection and without a name throw error
+    // if it is user first connection and without a name throw error
     if (!username) {
         return next(new Error('Please provide a valid username'))
     };
 
-    //At first connection set up username and user Id to be used for session
-
+    //At user first connection set up username and user Id to be used for session
     socket.username = username;
     socket.userId = socket.id;
-    // console.log('first Time', socket.userId)
+
+    //NOTE: session here could be a database.
     sessionStore.saveSession(socket.userId, { userId: socket.userId, username: socket.username })
-    // console.log('session store',sessionStore.getSession(socket.userId))
     next();
 })
-ioModule(io)
+
 chat(io)
 
 
