c509783fb67b5314c0ad96b62e321d9fd2fee756
Alalinga <alalinga.mubarik@amalitech.com>
2022-06-30 08:46:15 +0000

    refactor: added async await to all functions

--- src/api/controllers.js
+++ src/api/controllers.js
@@ -1,33 +1,74 @@
-const { default: user } = require("../utils/user")
+const { User } = require("../utils/user")
 
+const user = new User();
 
-
-const getUser = (req,res)=>{
+const getUser = async (req,res)=>{
     const id = req.params
-return user.getUser(id)
+    if(!id) res.json(new Error({error: "Please provide a user Id"})).status(400)
+    const response = await user.getUser(id);
+    //if(!user) res.json({NotFound: "User with the provided Id does not exist, Kindly check and try again"}).status(404)
+return res.json(response).status(response.code)
 }
 
-const getUserChats = (req,res)=>{
+const getUserChats = async (req,res)=>{
     const chatId = req.params
-    return user.getOneOnOneChat(chatId)
+    if(!chatId) return res.json(new Error({error:"Please provide a user Id"})).status(400)
+    const response = await user.getOneOnOneChat(chatId)
+    //if(chats === undefined) return res.json({chats: "No chats found for this user"}).status(404)
+    return res.json(response).status(response.code)
 }
 
-const getAllUsers = (req,res)=>{
-    return user.getAllUsers()
+const getAllUsers = async (req,res)=>{
+    const response = await user.getAllUsers();
+    //if(!users) return res.json({NoData:"Sorry, There are no user available"}).status(404)
+    return res.json(response).status(response.code)
 }
 
-const getGroupChats = (req,res)=>{
+const getGroupChats = async (req,res)=>{
     const groupId = req.params;
-    return user.getGroupChats(groupId)
+    if(!groupId) res.json(new Error({error:"Please, provide the group id or name"})).status(400)
+    const response = await user.getGroupChats(groupId)
+    //if(!groupChats) res.json({NotFound: "Sorry, there are no chats in this group"}).status(404)
+    return res.json(response).status(response.code)
 }
-const getAllGroupMembers = (req, res)=>{
+const getAllGroupMembers = async (req, res)=>{
     const groupId = req.params;
-    return user.getGroupUsers(groupId)
+    if(!groupId) res.json(new Error({error: "Please, provide the group id or name"})).status(400)
+    const response = await user.getGroupUsers(groupId);
+    //if(!groupMembers) res.json({NotFound: "Sorry, There are not members in this group"}).status(404)
+    return res.json(response).status(response.code)
 }
 
-const joinGroup=(req,res)=>{
-    const {username, groupName} = req.query
-    return user.addUserToGroup({username,groupName})
+const joinGroup= async (req,res)=>{
+    const {username, groupName} = req.query;
+    if(!username || !groupName) res.json(new Error({error: "Please, provide a valid username and a group name"})).status(400)
+    const response = await user.addUserToGroup({username,groupName})
+    return res.json(response).status(response.code)
+}
+const deleteGroup = async (req,res)=>{
+    const {groupName} = req.params;
+    if(!groupName) res.json(new Error({error: "Please, provide a group name"})).status(400);
+    const response = await user.deleteGroup(groupName)
+    return res.json(response).status(response.code)
+}
+const deleteUserFromGroup = async (req,res)=>{
+    const {groupName,userName} = req.params;
+    if(!groupName || !userName) res.json(new Error({error: "Please, provide a group and user name"})).status(400);
+    const response = await user.deleteUserFromGroup(groupName,userName)
+    return res.json(response).status(response.code)
+}
+const deleteGroupChats= async (req,res) =>{
+    const {groupName} = req.params;
+    if(!groupName) res.json(new Error({error: "Please, provide a group name"})).status(400);
+    const response = await user.deleteGroupChats(groupName)
+    return res.json(response).status(response.code)
 }
 
-module.exports = {getUser,getUserChats,joinGroup,getAllGroupMembers,getGroupChats,getAllUsers}
+const deleteUser = async (req,res)=>{
+    const {userName} = req.params;
+    if(!userName) res.json(new Error({error: "Please, provide a user name"})).status(400);
+    const response = await user.deleteUser(userName)
+    return res.json(response).status(response.code)
+}
+
+module.exports = {getUser,getUserChats,joinGroup,getAllGroupMembers,getGroupChats,getAllUsers,deleteGroup,deleteGroupChats,deleteUser,deleteUserFromGroup}
