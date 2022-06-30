f7aef063c8d28d22e6b546221224eeeb22f63bcb
Alalinga <alalinga.mubarik@amalitech.com>
2022-06-30 08:48:46 +0000

    build: imported all function from controller

--- src/api/chatRouters.js
+++ src/api/chatRouters.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const chatRouter = express.Router();
 const path = require('path');
-const { getUser,getUserChats,getAllUsers,joinGroup,getGroupChats,getAllGroupMembers } = require("./controllers");
+const { getUser,getUserChats,getAllUsers,joinGroup,getGroupChats,getAllGroupMembers, deleteGroup, deleteGroupChats, deleteUserFromGroup, deleteUser } = require("./controllers");
 const { loginUser } = require('./auth')
 
 
@@ -12,14 +12,24 @@ chatRouter.get('/test', (req, res) => {
 })
 
 chatRouter.post('/login',loginUser);
+
+// user routes
 chatRouter.get('user-details/:userId',getUser)
 chatRouter.get('user-chats/:userId',getUserChats)
 
 chatRouter.get('users/',getAllUsers)
+chatRouter.delete('users/delete/:userId',deleteUser)
 
+
+// group routes
 chatRouter.get('group-members/:groupId',getAllGroupMembers)
 chatRouter.get('group-chats/:groupId',getGroupChats)
+
 chatRouter.post('join-group/:groupId',joinGroup)
+
+chatRouter.delete('group/delete/:groupId',deleteGroup)
+chatRouter.delete('group-chats/delete/:groupId',deleteGroupChats)
+chatRouter.delete('group-members/delete/:groupId/:userId',deleteUserFromGroup);
 
 
 
