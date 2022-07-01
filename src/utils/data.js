2e6e0a29c35d2bad174e4de066a067379d0425bf
Alalinga <alalinga.mubarik@amalitech.com>
2022-07-01 10:54:25 +0000

    feat: added data to keep track of data storage

---
+++ src/utils/data.js
@@ -0,0 +1,10 @@
+        const allUsers = [];
+        const user = [];
+        const groups = []
+        const privateChat_Users = [];
+        const oneONoneChats = [];
+        const oneOneOneroomId = [];
+        const groupChats = [];
+
+
+        module.exports = {allUsers,user,groups,privateChat_Users,oneONoneChats,oneOneOneroomId,groupChats}
