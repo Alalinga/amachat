38356d0e0c24f2e38aa6cfb52a21563bc5490c64
Alalinga <alalinga.mubarik@amalitech.com>
2022-07-04 07:56:30 +0000

    refactor: changed node-time to default new Date

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,8 +1,14 @@
-const getDate = () => {
-    var dateTime = require('node-datetime');
-    var dt = dateTime.create();
-    var formatted = { date: dt.format('Y-m-d'), time: dt.format('H:M') };
-    return formatted
+const getDate = (datetime) => {
+    let month = datetime.getMonth()+1;
+    month = month<10? `0${month}`: month
+    let day = datetime.getDate();
+    day = day<10? `0${day}`:day
+    const date = `${datetime.getFullYear()}-${month}-${day}`
+    let minutes = datetime.getMinutes()
+    let hours = datetime.getHours()
+    minutes = minutes<10?`0${minutes}`: minutes;
+    const time = `${hours}:${minutes}`
+    return  { date, time };
 }
 
 module.exports = {getDate}
